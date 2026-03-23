dcfe3e52a1e98b45f6bb928b78685f20227772d6
Tallis Vinicius <tallis.vinicius03@gmail.com>
2026-03-23 19:28:59 -0300

    Nova atualização

--- server.js
+++ server.js
@@ -28,3 +28,29 @@ app.get('/incidentes', async (req, res) => {
 
     res.json(listaIncidentes);
 });
+
+
+app.get('/incidentes/:id', async (req, res) => {
+    const { id } = req.params
+
+    const db = await criarBanco()
+
+    const incidenteEspecifico = await db.all(`SELECT * FROM incidentes WHERE id = ?` , [id])
+
+    res.json(incidenteEspecifico);
+
+});
+
+app.post('/incidentes', async (req, res) => {
+
+    const {tipo_problema, localizacao, descricao, prioridade, nome_solicitante, data_registro,hora_registro} = req.body
+
+    const db = await criarBanco()
+
+    await db.run(`INSERT INTO incidentes(tipo_problema, localizacao, descricao, prioridade, nome_solicitante,
+        data_registro,hora_registro) VALUES (?, ?, ?, ?, ?, ?, ?)`, [tipo_problema, localizacao, descricao, prioridade, nome_solicitante,
+        data_registro,hora_registro])
+
+        res.send(`Incidente novo registrado: ${tipo_problema} registrado na data ${data_registro} por ${nome_solicitante}`)
+
+})
