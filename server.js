210c1111d69893ee23d1fa732196a30d68c9e1a2
Tallis Vinicius <tallis.vinicius03@gmail.com>
2026-03-30 19:23:49 -0300

    configura CORS e porta dinâmica para deploy

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express  = require ('express');
 const { criarBanco } = require ('./database');
 
+const cors = require("cors")
+
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
@@ -16,10 +20,7 @@ app.get('/', (req, res) => {
         `);
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(` Servidor rodando em http://localhost:${PORT}`)
-});
+
 
 app.get('/incidentes', async (req, res) => {
     const db = await criarBanco();
@@ -80,4 +81,10 @@ app.delete('/incidentes/:id', async(req, res) => {
         `, [id])
 
         res.send(` O incidentes de ${id} foi removido com sucesso `)
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(` Servidor rodando em http://localhost:${PORT}`)
 });
